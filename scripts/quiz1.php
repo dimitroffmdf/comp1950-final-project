22994ef0a28a0838fd714f3046d7bdc3a601589d
Milen Dimitrov <dimitroffmdf@gmail.com>
2017-06-27 23:36:14 -0700

    quiz1.html + quiz.php

---
+++ scripts/quiz1.php
@@ -0,0 +1,15 @@
+<h1>Thank you for your response. Your quiz is sent for review</h1>
+<hr />
+Name: <?php echo $_POST['studentname']; ?> <br />
+Student Number: <?php echo $_POST['studentnumber']; ?><br />
+<hr />
+<h2>Your Answers</h2>
+Question 1: <?php echo $_POST['question-one']; ?><br />
+Question 2: <?php echo $_POST['question-two']; ?><br />
+Question 3: <?php echo $_POST['question-three']; ?><br />
+Question 4: <?php echo $_POST['question-four']; ?><br />
+Question 5: <?php echo $_POST['question-five']; ?><br />
+<br />
+<br />
+<a href="/">return back to the page</a>
+
